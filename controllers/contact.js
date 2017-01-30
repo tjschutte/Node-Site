3d37c3c22f641176023ec6f3e58ef26b498ee5d1
Tom Schutte <tjschutte@wisc.edu>
2017-01-30 12:24:34 -0600

    configuring node mailer
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,3 +1,9 @@
+
+const config = require('../config.json');
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport('smtps://' + config.email + ':' + config.email_password + '@smtp.gmail.com');
+
 /**
  * GET /
  * contact page.
@@ -13,8 +19,23 @@ exports.getContact = (req, res) => {
 */
 exports.postContact = (req, res) => {
   console.log('POST: /contact');
+
+  var mailOptions = {
+    from: req.body.name.val + '<' + req.body.email.val + '>', // sender address
+    to: 'tjschutte@wisc.edu', // list of receivers
+    subject: 'From-Site', // Subject line
+    text: req.body.message.val, // plaintext body
+  };
+
   console.log(req.body.name.val);
   console.log(req.body.email.val);
   console.log(req.body.message.val);
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
   res.redirect('..');
 }
