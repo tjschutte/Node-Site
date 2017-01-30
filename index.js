a8c7af8eb00e7451656ff71bf293ec80f5945ea3
Tom Schutte <tjschutte@wisc.edu>
2017-01-30 18:33:16 +0000

    Fixups for self hosting
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const projectsController = require('./controllers/projects');
  * Create Express server.
  */
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
